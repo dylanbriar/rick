9e4f18742364b0b83194709a6d981bdbf0303314
Dylan Briar <dylancomptontx@gmail.com>
2023-12-21 08:24:27 -0600

    latest version
    added after scroll functionality

---
+++ fido.js
@@ -0,0 +1,12 @@
+// class Dog {
+//   constructor(el) {
+//     this.node = document.createElement('img');
+//     this.node.setAttribute('id', 'fido');
+//     this.node.setAttribute('src', 'assets/happypup.png');
+
+//     el.appendChild(this.node);
+
+//     this.node.style.left = `0px`;
+//     this.node.style.top = `0px`;
+//   }
+// }
--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const allURLS = document.links;
-for(i=0;i<allURLS.length;i++) {
-    allURLS[i].href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+function rickPlace (){
+    const allURLS = document.links;
+    for(i = 0; i < allURLS.length; i++) {
+        allURLS[i].href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
 }
+rickPlace()
 
+document.addEventListener('scroll', () => {
+    rickPlace()
+})
